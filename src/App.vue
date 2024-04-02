5a4b78e61c73aff53595b01043ae8148bbee8928
on22vy <thi.nguyen.22@lehre.mosbach.dhbw.de>
2024-04-02 03:19:12 +0200

    Added ContentCreator Component

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,18 @@
-<script>
-  import { QuillEditor } from '@vueup/vue-quill'
-  import '@vueup/vue-quill/dist/vue-quill.snow.css';
-
-  export default {
-    components: {
-      QuillEditor
-    }
-  }
-</script>
-
 <template>
-  <QuillEditor toolbar="full" />
+  <div id="app">
+    <content-creator></content-creator>
+  </div>
 </template>
 
+<script>
+import ContentCreator from './components/ContentCreator.vue'
 
-<style>
-nav {
-  padding: 30px;
+export default{
+  name: 'app',
+  components: {
+    ContentCreator
+  }
+
 }
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
-
+</script>
+<style></style>
