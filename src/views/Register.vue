54735c0f42008492740fead4989c808d63e393eb
Lorena <lorenah1301@gmail.com>
2024-04-03 14:14:15 +0200

    added css, images and logo as well as code formatting

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,31 +1,53 @@
 <template>
+  <div class="navBar">
+    <img src="../assets/NotizenAppLogo.svg" alt="logo" class="logo">
+  </div>
+
+  <div class="content">
+
     <h1>Create an Account</h1>
-    <p><input type="text" placeholder="Email" v-model="email" /></p>
-    <p><input type="password" placeholder="Password" v-model="password" /></p>
-    <p><button @click="register">Submit</button></p>
-    <p><button @click="signIn">Melde mich an</button></p>
-    <p><button @click="signInWithGoogle">Sign In With Google</button></p>
+    <div>
+      <img src="../assets/img.png" class="imgLogin">
+    </div>
+
+    <div>
+      <p><input class="loginInput" type="text" placeholder="Email" v-model="email" /></p>
+      <p><input class="loginInput" type="password" placeholder="Password" v-model="password" /></p>
+    </div>
+
+    <div class="btnContainer">
+      <p class="btns submitBtn"><button @click="register">Submit</button></p>
+      <p class="btns"><button @click="signIn">Melde mich an</button></p>
+      <p class="btns"><button @click="signInWithGoogle">Sign In With Google</button></p>
+    </div>
+
+  </div>
+
 </template>
 
 <script setup>
 import { ref } from "vue";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { useRouter } from 'vue-router' // import router
+
+// css import
+import '../assets/login.css';
+
 const email = ref("");
-const password = ref(""); 
+const password = ref("");
 const router = useRouter() // get a reference to our vue router
 
 const register = () => {
-    // need value because ref()
-createUserWithEmailAndPassword(getAuth(), email.value, password.value)
-  .then((data) => {
-console.log("Successfully registered!");
-router.push('/') // redirect to the feed
-})
-.catch((error) => {
-console.log(error.code);
-alert(error.message);
-});
+  // need value because ref()
+  createUserWithEmailAndPassword(getAuth(), email.value, password.value)
+    .then((data) => {
+      console.log("Successfully registered!");
+      router.push('/') // redirect to the feed
+    })
+    .catch((error) => {
+      console.log(error.code);
+      alert(error.message);
+    });
 
 }
 const signIn = () => {
--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,18 +1,34 @@
 <template>
 
-    <nav>
-        <router-link to="/"> Sign In </router-link> |
-        <!-- <router-link to="/feed"> Feed </router-link> | -->
-        <router-link to="/register"> Register </router-link> |
-        <!-- <router-link to="/sign-in"> Login </router-link> | -->
-    </nav>
-    <p><button @click="home">Home</button></p>
+    <div class="navBar">
+        <img src="../assets/NotizenAppLogo.svg" alt="logo" class="logo">
+    </div>
+
+    <div class="content">
+        <nav>
+            <router-link to="/"> Sign In </router-link> |
+            <!-- <router-link to="/feed"> Feed </router-link> | -->
+            <router-link to="/register"> Register </router-link> |
+            <!-- <router-link to="/sign-in"> Login </router-link> | -->
+        </nav>
+
+        <div>
+            <img src="../assets/img.png" class="imgLogin">
+        </div>
+
+        <p><button @click="home">Home</button></p>
+    </div>
+
 </template>
 
 <script setup>
-    import { useRouter } from 'vue-router'
-    const router = useRouter()
-    const home = () => {
+import { useRouter } from 'vue-router'
+
+// css import
+import '../assets/login.css';
+
+const router = useRouter()
+const home = () => {
     router.push('/home')
-    }
+}
 </script>
