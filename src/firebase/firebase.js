3c50b299108e862320c31aae19e5e655b1584a42
on22vy <thi.nguyen.22@lehre.mosbach.dhbw.de>
2024-04-03 12:37:18 +0200

    Firebase Config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,18 +1,30 @@
 import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore';
+// import { getFirestore } from 'firebase/firestore';
+import { getDatabase, ref, set } from "firebase/database";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyBSMxKueWaa5cEMtWZB7O-FAxDjQSI6URM',
-  authDomain: 'notizen-app-pwa-4afb6.firebaseapp.com',
-  projectId: 'notizen-app-pwa-4afb6',
-  storageBucket: 'notizen-app-pwa-4afb6.appspot.com',
-  messagingSenderId: '896099446638',
-  appId: '1:896099446638:web:27f5d3e43a743ccff7db7f'
+  apiKey: "AIzaSyBSMxKueWaa5cEMtWZB7O-FAxDjQSI6URM",
+  authDomain: "notizen-app-pwa-4afb6.firebaseapp.com",
+  databaseURL: "https://notizen-app-pwa-4afb6-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "notizen-app-pwa-4afb6",
+  storageBucket: "notizen-app-pwa-4afb6.appspot.com",
+  messagingSenderId: "896099446638",
+  appId: "1:896099446638:web:27f5d3e43a743ccff7db7f"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
-const db = getFirestore(app)
+// const db = getFirestore(app)
+
+  const db = getDatabase(app);
+  const reference = ref(db, 'notes/'+ 'data/')
+  
+  set(reference,{
+    content: content
+  })
+
+writeData("hi")
 
 export{
     db
