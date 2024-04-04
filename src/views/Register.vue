1c8189173e6269102523e1c956660e19b885dcf1
on22vy <thi.nguyen.22@lehre.mosbach.dhbw.de>
2024-04-04 18:30:53 +0200

    Register.vue: Added author tag and Error messages for Registration

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,3 +1,60 @@
+<script setup>
+/**
+ * @author Marie Bleickert <github: MaryScarBit>
+ */
+
+import { ref } from "vue";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { useRouter } from 'vue-router' // import router
+
+// css import
+import '../assets/login.css';
+
+const email = ref("");
+const password = ref("");
+const errMsg = ref("");
+
+const router = useRouter() // get a reference to our vue router
+
+const register = () => {
+  // need value because ref()
+  createUserWithEmailAndPassword(getAuth(), email.value, password.value)
+    .then((data) => {
+      console.log("Successfully registered!");
+      router.push('/') // redirect to the feed
+    })
+    .catch((error) => {
+      // console.log(error.code);
+      // alert(error.message);
+
+      console.log(error.code);
+            switch (error.code) {
+                case "auth/missing-password":
+                    errMsg.value = "Please enter a password";
+                    break;
+                case "auth/missing-email":
+                    errMsg.value = "Please enter an valid e-mail to register";
+                    break;
+                case "auth/weak-password":
+                    errMsg.value = "Password should be at least 6 characters";
+                    break;
+                case "auth/email-already-in-use":
+                    errMsg.value = "Sorry, this email address is already in use, please enter another email address";
+                    break;
+                default:
+                    errMsg.value = "Error: Something went wrong. User Registration failed";
+                    break;
+            }
+            alert(error.message);
+    });
+
+}
+const signIn = () => {
+  router.push('/')
+}
+
+</script>
+
 <template>
   <div class="navBar">
     <img src="../assets/NotizenAppLogo.svg" alt="logo" class="logo">
@@ -22,11 +79,11 @@
     <div>
       <p><input class="loginInput" type="text" placeholder="Email" v-model="email" /></p>
       <p><input class="loginInput" type="password" placeholder="Password" v-model="password" /></p>
+      <p v-if="errMsg">{{ errMsg }}</p>
     </div>
 
     <div class="btnContainer">
-      <p class="btns submitBtn"><button @click="register">Anmelden</button></p>
-      <p class="btns"><button @click="signInWithGoogle">Mit Google anmelden</button></p>
+      <p class="btns submitBtn"><button @click="register">Registrieren</button></p>
     </div>
 
 
@@ -34,35 +91,3 @@
 
 </template>
 
-<script setup>
-import { ref } from "vue";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { useRouter } from 'vue-router' // import router
-
-// css import
-import '../assets/login.css';
-
-const email = ref("");
-const password = ref("");
-const router = useRouter() // get a reference to our vue router
-
-const register = () => {
-  // need value because ref()
-  createUserWithEmailAndPassword(getAuth(), email.value, password.value)
-    .then((data) => {
-      console.log("Successfully registered!");
-      router.push('/') // redirect to the feed
-    })
-    .catch((error) => {
-      console.log(error.code);
-      alert(error.message);
-    });
-
-}
-const signIn = () => {
-  router.push('/')
-}
-
-const signInWithGoogle = () => {
-}
-</script>
