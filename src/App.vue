93071c0ea9c66bf4992b08bdc26d80078367dc80
on22vy <thi.nguyen.22@lehre.mosbach.dhbw.de>
2024-04-04 17:21:16 +0200

    remove commented/unused code

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,6 @@
-<!-- <template>
-
-  <nav>
-<router-link to="/"> Home </router-link> |
-<router-link to="/feed"> Feed </router-link> |
-<router-link to="/register"> Register </router-link> |
-<router-link to="/sign-in"> Login </router-link> |
-</nav>
-  <router-view />
-  <QuillEditor toolbar="full" />
-</template> -->
-<!-- <script>
-  import { QuillEditor } from '@vueup/vue-quill'
-  import '@vueup/vue-quill/dist/vue-quill.snow.css';
-  import './assets/toolbar.css'; 
-  // import downloadBtn from './components/downloadBtn.vue';
-
-</script> -->
 <script setup>
-/*
- * @author Thi Tuong Vy Nguyen <thi.nguyen.22@lehre.mosbach.dhbw.de>
+/**
+ * @author Thi Tuong Vy Nguyen <github: on22vy>
  */
 
 </script>
--- src/router/router.js
+++ src/router/router.js
@@ -1,24 +1,11 @@
 /**
  * @author Thi Tuong Vy Nguyen <thi.nguyen.22@lehre.mosbach.dhbw.de>
  */
+
 import { createRouter, createWebHistory} from "vue-router";
 import SignIn from '../views/SignIn.vue'
 import Register from '../views/Register.vue'
 import Home from '../views/Home.vue'
-
-
-// const router = createRouter({
-//     history: createWebHistory(),
-//     routes:[
-//     { path: "/", component: () => import("../views/Home.vue") },
-//     { path: "/register", component: () => import("../views/Register.vue")},
-//     { path: "/sign-in", component: () => import("../views/SignIn.vue") },
-//     { path: "/feed", component: () => import("../views/Feed.vue") },
-// ],
-// });
-//     export default router;
-
-
 
 const router = createRouter({
     history: createWebHistory(),
